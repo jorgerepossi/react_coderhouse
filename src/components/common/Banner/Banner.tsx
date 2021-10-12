7285d5704b79bc0138410bf768d3c952da828aba
Jorge Repossi <jorgerepossi1980@gmail.com>
2021-10-12 18:24:07 -0300

    https

--- src/components/common/Banner/Banner.tsx
+++ src/components/common/Banner/Banner.tsx
@@ -6,7 +6,7 @@ interface Props {}
 export const Banner: FC<Props> = () => {
   return (
     <Image
-      src="http://jrepossi.com/musiccenter/img/bannerSlide.jpeg"
+      src="https://www.verbostudio.com/musiccenter/img/bannerSlide.jpeg"
       width="100%"
     />
   )
